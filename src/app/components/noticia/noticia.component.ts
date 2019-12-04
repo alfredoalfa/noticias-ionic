2007bff87024343a1e5570726f61ff87c903ef25
alfredo contreras <alfredolcontrerasl@gmail.com>
2019-12-04 10:11:32 -0400

    added share news

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-noticia',
@@ -11,8 +14,48 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number;
 
-  constructor() { }
+  constructor(private iab: InAppBrowser, public actionSheetController: ActionSheetController, private socialSharing: SocialSharing) { }
 
   ngOnInit() {}
+
+  openNew() {
+      console.log('Noticia', this.noticia);
+      const browser = this.iab.create(this.noticia.url);
+
+  }
+  async launchMenu() {
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [{
+        text: 'Share',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+          this.socialSharing.share (
+              this.noticia.title,
+              this.noticia.source.name,
+              '',
+              this.noticia.url,
+          );
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 
 }
